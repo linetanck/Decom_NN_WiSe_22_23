ef140cf70599571e2df81835e707355b47b430f8
linetanck <123452320+linetanck@users.noreply.github.com>
2023-03-15 17:06:07 +0100

    Update keras_attempt.py

--- Abgabe/keras_attempt.py
+++ Abgabe/keras_attempt.py
@@ -21,14 +21,14 @@ target_data = target_data.values
 encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=True)
 encoded_tokens = encoder.fit_transform(train_data).toarray()
 
-# separate encoded tokens and target rating into separate numpy arrays
+# separates encoded tokens and target rating into separate numpy arrays
 encoded_tokens_array = np.array(encoded_tokens)
 target_data_array = np.array(target_data)
 
-# create dataset as a tuple of encoded tokens array and target rating array
+# creates dataset as a tuple of encoded tokens array and target rating array
 train_dataset = (encoded_tokens_array, target_data_array)
 
-# define input layer
+# defines input layer
 input_layer = Input(shape=(encoded_tokens_array.shape[1],))
 
 # add embedding layer to convert input to the shape expected by ResNet50
